1bf051531f04ce50c49d075168cfbb43a274a401
ReinaldiRS <reinaldiarmstrong@ymail.com>
2019-10-04 17:18:53 +0700

    revisi1

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 $('#banner').text("Hello, JQuery");
 
-function sapadia(){
-    var nama = $('#nama').val();
-    $('#banner').text(`hey there, ${nama}!`);
+function tambahkan(){
+    var angka_pertama =parseInt($('#angka_pertama').val());
+    var angka_kedua =parseInt($('#angka_kedua').val());
+
+    var hasil = angka_pertama + angka_kedua;
+
+    $('#hasil').text(`Hasil ${angka_pertama} + 
+    ${angka_kedua} = ${hasil}`)
 }
